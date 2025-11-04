69d39eeda7ad836cf3e02a7fa7e125323ee4c3f1
7Louieee <hed-jldalapo@smu.edu.ph>
2025-11-04 18:22:55 +0800

    event handler for StreamWriter with error handling and beautiful message box :))

--- FileProcessing/Form1.vb
+++ FileProcessing/Form1.vb
@@ -1,3 +1,25 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+
+        Try
+
+            Dim filePath As String = "sample.txt"
+
+
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine("Hello, this is a test file")
+                writer.WriteLine("Second Line of Text")
+            End Using
+
+            MessageBox.Show("Data written successfully.", "Sucess!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error" & ex.Message, "Error Has Occured!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+
+        End Try
+
+    End Sub
 End Class
